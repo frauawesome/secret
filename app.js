afc3115cac50c7cc3b4b7044447c05731faf7fd1
Carly Schmitting <cschmitting@gmail.com>
2021-01-24 16:45:03 +0100

    User can post a message

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema ({ 
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -60,8 +61,6 @@ passport.use(new GoogleStrategy({
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -92,16 +91,28 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-    if (req.isAuthenticated()){
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if (err){
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    });
 });
 
 app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/");
+});
+
+app.get("/submit", function(req, res){
+    if (req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
 });
 
 app.post("/register", function(req, res){
@@ -135,7 +146,22 @@ app.post("/login", function(req, res){
     });
 });
 
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
 
+    User.findById(req.user.id, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
